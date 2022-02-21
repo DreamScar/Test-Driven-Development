8b59356cc69f2b97a39ddd849ab5a0accb72b6d8
DreamScar <zjmeng666666@gmail.com>
2022-02-21 14:56:29 +0800

    Second Refactor to eliminate repeat

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         #伊迪丝听说有个很酷的 在线待办事项应用
         #她去看了这个应用的首页
@@ -34,10 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         #待办事项表格中显示了“1: Buy peacock feathers”      
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), f"New to-do item did not appear in table. Content were:\n{table.text}")
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #页面中又显示了一个文本框，可以输入其他的待办事项
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -50,10 +52,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         #页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         self.fail('Finish the test!')
 
